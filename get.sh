bc073d198e53a4a8ff6e5a84ad952c015bcc81f0
Rubin Bhandari <roobin.bhandari@gmail.com>
2021-08-10 14:22:17 +0545

    Commit number 1628584637-1

--- get.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-for((i=1;i<=1000000;i++)); do
-        content=$(date -d "${i}" +"%s")
-        echo ${content}-${i} >> .commits/changes
-        git add .commits/changes
-        git commit -m "Commit number ${i}-${i}"
-        git commit --amend --no-edit
-done
